1c02d4a9469971618ac12c409e721e3dd15a2900
sandeep <nanisandeep876@gmail.com>
2025-04-07 23:25:43 +0530

    initilize GCP repo

---
+++ Bigquery/sample.sql
@@ -0,0 +1,3 @@
+--- distinct count of a column
+select count(distinct spotify_track_uri) from tsda-456117.RDL.SPOTIFY
+
---
+++ SDK/cmd.sh
@@ -0,0 +1,6 @@
+gcloud config list -- list all configurations
+gcloud config set project PROJECT_ID  -- set the default project
+gcloud config set account ACCOUNT  -- set the default account
+gcloud config set compute/region REGION  -- set the default region
+gcloud config set compute/zone ZONE  -- set the default zone
+gcloud compute instances list -- list all instances
